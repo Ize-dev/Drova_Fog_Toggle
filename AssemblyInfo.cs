5ce78617a9480b5ee82b996d4474f72d9a2e553c
Marco <61373888+Ize-dev@users.noreply.github.com>
2024-11-04 18:00:15 +0100

    initial commit

---
+++ AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using MelonLoader;
+using Drova_Fog_Toggle;
+
+[assembly: MelonInfo(typeof(FogToggle), "Drova Fog Toggle", "1.0.0", "Ize", null)]
+[assembly: MelonGame("Just2D", "Drova")]
---
+++ Class1.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using MelonLoader;
+using Il2Cpp;
+using Il2CppDrova;
+using UnityEngine.Windows;
+
+
+namespace Drova_Fog_Toggle
+{
+    public class FogToggle : MelonMod
+    {
+        public GameObject fog = null;
+        public override void OnInitializeMelon()
+        {
+            Melon<FogToggle>.Logger.Msg("Fog toggler active.");
+        }
+        //Floor Fog
+        public override void OnLateUpdate()
+        {
+            bool myInput = UnityEngine.Input.GetKeyDown(KeyCode.N);
+            if (myInput)
+            {
+                if (fog == null)
+                {
+                    fog = GameObject.Find("Floor Fog");
+                }
+                fog.SetActive(!fog.active);
+            }
+        }
+
+    }
+}
